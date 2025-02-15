b6afa80cc0fc46a281da621c8e95852e7f705db9
Shankar-Sala <shankarsala9995@gmail.com>
2025-02-15 18:03:04 +0530

    update

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -46,6 +46,17 @@ const validateListing = (req, res, next)=>{
     }
 }
 
+const validateReview = (req, res, next)=>{
+  let {error} = reviewSchema.validate(req.body);
+
+    if(error){
+      let errMsg =error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400, errMsg);
+    }else{
+      next();
+    }
+}
+
 //Index Route
 app.get(
   "/listings",
@@ -65,7 +76,7 @@ app.get(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
   })
 );
@@ -111,6 +122,30 @@ app.delete(
   })
 );
 
+//Reviews
+//Post Route
+app.post("/listings/:id/reviews",validateReview, wrapAsync(async(req, res) => {
+let listing = await Listing.findById(req.params.id);
+let newReview = new Review(req.body.review);
+
+listing.reviews.push(newReview);
+
+await newReview.save();
+await listing.save();
+
+res.redirect(`/listings/${listing._id}`);
+}));
+
+// Delete Route
+app.delete(
+  "/listings/:id/reviews/:reviewId", wrapAsync(async (req,res) => {
+    let { id, reviewId} = req.params;
+
+    await listing.findByIdAndUpdate(id, {$pull: {reviews:reviewId }});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  }));
 // app.get("/testListing", async (req, res) => {
 //   try {
 //     let sampleListing = new Listing({
--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js")
 
 const listingSchema = new Schema({
   title: {
@@ -16,6 +17,18 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews:[
+    {
+      type:Schema.Types.ObjectId,
+      ref:"Review"
+    }
+  ]
+});
+
+listingSchema.post("findOneAndDelete",async (listing) => {
+   if(listing) {
+    await Review.deleteMany({reviews: {$in: listing.review}});
+   };
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
